087d521e5ad010dfe0fbe3445750d663d51fb284
amalamine <amalamine00@gmail.com>
2019-10-01 15:08:16 +0400

    init

---
+++ src/main/java/test/ecobank/microservice/MicroserviceApplication.java
@@ -0,0 +1,26 @@
+package test.ecobank.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+
+	@RestController
+	public class HealthController {
+
+		@GetMapping("/health")
+		public String health() {
+			return "Up";
+		}
+	}
+
+}
